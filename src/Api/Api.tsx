53cda651b9f37c07fabf00035a4df2a823a33b9c
ThomasBfrd <thomas.bfrd@gmail.com>
2025-01-10 15:24:18 +0100

    feat: mise à jour des méthodes des récupérations de données (local et api)

--- src/Api/Api.tsx
+++ src/Api/Api.tsx
@@ -21,3 +21,78 @@ export const getUserInfos = async (type: string) => {
         })
     }
 }
+
+// Get Activity
+export const getUserActivity = async (type: string) => {
+    if(type === 'api') {
+        return fetch('http://localhost:3000/user/12/activity')
+        .then((response: Response) => {
+            if (!response.ok) {
+                throw (new Error())
+            }
+            
+            
+            return response.json()
+        })
+    } else if (type === 'local') {
+
+        return fetch('/user-activity.json')
+        .then((response: Response) => {
+            if (!response.ok) {
+                throw (new Error())
+            }
+    
+            return response.json()
+        })
+    }
+}
+
+// Get Average Sessions
+export const getAverageSessions = async (type: string) => {
+    if(type === 'api') {
+        return fetch('http://localhost:3000/user/12/average-sessions')
+        .then((response: Response) => {
+            if (!response.ok) {
+                throw (new Error())
+            }
+            
+            
+            return response.json()
+        })
+    } else if (type === 'local') {
+
+        return fetch('/user-average-sessions.json')
+        .then((response: Response) => {
+            if (!response.ok) {
+                throw (new Error())
+            }
+    
+            return response.json()
+        })
+    }
+}
+
+// Get Performances
+export const getPerformances = async (type: string) => {
+    if(type === 'api') {
+        return fetch('http://localhost:3000/user/12/performance')
+        .then((response: Response) => {
+            if (!response.ok) {
+                throw (new Error())
+            }
+            
+            
+            return response.json()
+        })
+    } else if (type === 'local') {
+
+        return fetch('/user-performance.json')
+        .then((response: Response) => {
+            if (!response.ok) {
+                throw (new Error())
+            }
+    
+            return response.json()
+        })
+    }
+}
