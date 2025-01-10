a898c6c88f48458c684b361ef38113cc3e8131de
ThomasBfrd <thomas.bfrd@gmail.com>
2025-01-10 15:26:18 +0100

    feat: récupération et unification du user selon le dataSource

--- src/features/pages/Dashboard.tsx
+++ src/features/pages/Dashboard.tsx
@@ -1,83 +1,66 @@
 import { useEffect, useState } from "react";
-import { useDispatch, useSelector, useStore } from "react-redux";
-import { getUsersList } from "../../app/core/selectors";
+import { useDispatch, useSelector } from "react-redux";
+import { AppDispatch } from "../../app/core/store";
+import { selectDataSource, selectUsers } from "../../app/core/selectors";
 import ActivityGraph from "../components/activity/ActivityGraph";
 import "./Dashboard.scss";
 import Score from "../components/score/Score";
 import Performances from "../components/performances/Performances";
 import Card from "../components/card/Card";
 import AverageSessions from "../components/average-sessions/AverageSessions";
-import { RootState } from "@reduxjs/toolkit/query";
-import { getUserInfos } from "../../Api/Api";
-import { User } from "../../app/core/interfaces/user.interface";
-
-const data = [
-  {
-    image: "/public/protein-icon.png",
-    number: "155g",
-    text: "Protéines",
-  },
-  {
-    image: "/public/carbs-icon.png",
-    number: "290g",
-    text: "Glucides",
-  },
-  {
-    image: "/public/fat-icon.png",
-    number: "50g",
-    text: "Lipides",
-  },
-  {
-    image: "/public/calories-icon.png",
-    number: "50g",
-    text: "Calories",
-  },
-];
+import { switchDataSource } from "../../app/core/userSlice";
+import { User } from "../../app/core/interfaces/user";
 
 export default function Dashboard() {
-  const getUsers = useSelector(getUsersList);
-  const [fetchData, setFetchData] = useState(false);
-  const [isDataFetched, setIsDataFetched] = useState(false);
-  const store = useStore<RootState>();
-  const dispatch = useDispatch();
-
-  const checkDataFetchedBtn = () => {
-      const fetchData = async () => {
-        const data = await getUserInfos('api');
-
-        if (!isDataFetched && data) {
-          setIsDataFetched(true);
-          console.log(data.data);
-          
-          // const existingUsers: Array<User> = store.getState().users;
-          // const uniqueUsers = data.data.filter(
-          //   (user: User) => !existingUsers.includes(user)
-          // );
-          // console.log(uniqueUsers);
-
-          dispatch({
-            type: "GET_API_USER",
-            payload: data,
-          });
-        }
-        return () => {};
-      };
-
-      fetchData();
-  }
+  const dispatch: AppDispatch = useDispatch();
+  const users = useSelector(selectUsers);
+  const dataSource = useSelector(selectDataSource);
+  const [userFromStore, setUserFromStore] = useState<User>();
 
   useEffect(() => {
+    const getUserFromStore = () => {
+      if (dataSource === 'local' && users.localUser) {
+        const {data, activity, averageSession, performances} = users.localUser;
 
-    if (fetchData) {
-      checkDataFetchedBtn();
+        if (data && activity && averageSession && performances) {
+          console.log('envoi des données locales aux composants');
+          setUserFromStore({
+            userInfos: data,
+            userActivity: activity,
+            userPerformances: performances,
+            userAverage: averageSession
+          });
+        }
+
+      } else if (dataSource === 'api') {
+        const {data, activity, averageSession, performances} = users.apiUser;
+
+        if (data && activity && averageSession && performances) {
+          console.log('envoi des données API aux composants');
+          setUserFromStore({
+            userInfos: data,
+            userActivity: activity,
+            userPerformances: performances,
+            userAverage: averageSession
+          });
+        }
+      }
+    }
+
+    if (!userFromStore) {
+      getUserFromStore();
     }
 
     return () => {};
-  }, [getUsers, fetchData]);
+  }, [dataSource, dispatch, userFromStore, users.apiUser, users.localUser]);
+
+  const handleSwitch = () => {
+    dispatch(switchDataSource());
+  }
 
   return (
     <div className="dashboard">
-      {getUsers ? (
+        {users && userFromStore ? (
         <>
           <div className="dashboard__header">
             <div className="resume">
@@ -85,47 +68,37 @@ export default function Dashboard() {
                 <h1>
                   Bonjour{" "}
                   <span className="name">
-                    {getUsers[0]?.userInfos?.firstName}
+                    {userFromStore.userInfos.userInfos.firstName}
                   </span>
                 </h1>
               </div>
               <p>Félicitations ! Vous avez explosé vos objectifs hier 👏</p>
             </div>
             <div className="switch">
-              <input type="checkbox" id="switch" checked={fetchData} onChange={() => setFetchData(fetchData => !fetchData)}/>
+              <input type="checkbox" id="switch" checked={dataSource === 'api'} onChange={handleSwitch}/>
               <label htmlFor="switch"></label>
-              Données API
+              Données {dataSource === 'local' ? 'locales' : 'API'}
             </div>
           </div>
           <div className="ladderboard">
             <div className="cards">
-              <ul className="card-list">
-                {data.map((card, index) => (
-                  <li key={index} className="card-item">
-                    <Card {...card} />
-                  </li>
-                ))}
-              </ul>
+              <Card userKeyData={userFromStore.userInfos.keyData} />
             </div>
             <div className="activity">
-              <ActivityGraph />
+              <ActivityGraph user={userFromStore.userActivity} />
             </div>
             <div className="average-sessions">
-              <AverageSessions />
+              <AverageSessions user={userFromStore.userAverage}/>
             </div>
             <div className="performances">
-              <Performances />
+              <Performances user={userFromStore.userPerformances} />
             </div>
             <div className="score">
-              <Score />
+              <Score user={userFromStore.userInfos.todayScore} />
             </div>
           </div>
         </>
-      ) : (
-        <div className="dashboard__header">
-          <span>Aucun utilisateur trouvé</span>
-        </div>
-      )}
+        ): <div>Les données de l'utilisateur ne sont pas disponibles pour le moment.</div>}
     </div>
   );
 }
