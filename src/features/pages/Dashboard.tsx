c26c90efacaf7c340314603c061773c9d4e1bc83
ThomasBfrd <thomas.bfrd@gmail.com>
2025-01-17 20:01:01 +0100

    fix: switch des données locales avec api si bouton actif

--- src/features/pages/Dashboard.tsx
+++ src/features/pages/Dashboard.tsx
@@ -1,7 +1,7 @@
 import { useEffect, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { AppDispatch } from "../../app/core/store";
-import { selectDataSource, selectUsers } from "../../app/core/selectors";
+import { selectDataSource, selectStatus, selectUsers } from "../../app/core/selectors";
 import ActivityGraph from "../components/activity/ActivityGraph";
 import "./Dashboard.scss";
 import Score from "../components/score/Score";
@@ -15,28 +15,17 @@ export default function Dashboard() {
   const dispatch: AppDispatch = useDispatch();
   const users = useSelector(selectUsers);
   const dataSource = useSelector(selectDataSource);
+  const status = useSelector(selectStatus);
   const [userFromStore, setUserFromStore] = useState<User>();
+  const [isChecked, setIsChecked] = useState(dataSource === 'api');
 
   useEffect(() => {
     const getUserFromStore = () => {
-      if (dataSource === 'local' && users.localUser) {
-        const {data, activity, averageSession, performances} = users.localUser;
-
+      const user = dataSource === 'local' ? users.localUser : users.apiUser;
+      
+      if (user) {
+        const { data, activity, averageSession, performances } = user;
         if (data && activity && averageSession && performances) {
-          console.log('envoi des données locales aux composants');
-          setUserFromStore({
-            userInfos: data,
-            userActivity: activity,
-            userPerformances: performances,
-            userAverage: averageSession
-          });
-        }
-
-      } else if (dataSource === 'api') {
-        const {data, activity, averageSession, performances} = users.apiUser;
-
-        if (data && activity && averageSession && performances) {
-          console.log('envoi des données API aux composants');
           setUserFromStore({
             userInfos: data,
             userActivity: activity,
@@ -45,60 +34,86 @@ export default function Dashboard() {
           });
         }
       }
-    }
+    };
+    getUserFromStore();
 
-    if (!userFromStore) {
-      getUserFromStore();
+    if (status === 'failed') {
+      setIsChecked(false);
     }
 
     return () => {};
-  }, [dataSource, dispatch, userFromStore, users.apiUser, users.localUser]);
+  }, [dataSource, dispatch, users.apiUser, users.localUser, status, setIsChecked]);
 
   const handleSwitch = () => {
+    setIsChecked(!isChecked);
     dispatch(switchDataSource());
+  }
+
+  const renderContent = () => (
+    <>
+    {userFromStore ? (
+      <>
+        <div className="dashboard__header">
+          <div className="resume">
+            <div className="infos">
+              <h1>
+                Bonjour 
+                <span className="name"> 
+                  {userFromStore.userInfos.userInfos.firstName}
+                </span>
+              </h1>
+            </div>
+            <p>Félicitations ! Vous avez explosé vos objectifs hier 👏</p>
+          </div>
+          <div className="switch">
+            <input type="checkbox" id="switch" checked={isChecked} onChange={handleSwitch}/>
+            <label htmlFor="switch"></label>
+            Données {dataSource === 'local' ? 'locales' : 'API'}
+          </div>
+        </div>
+        <div className="ladderboard">
+          <div className="cards">
+            <Card userKeyData={userFromStore.userInfos.keyData} />
+          </div>
+          <div className="activity">
+            <ActivityGraph user={userFromStore.userActivity} />
+          </div>
+          <div className="average-sessions">
+            <AverageSessions user={userFromStore.userAverage}/>
+          </div>
+          <div className="performances">
+            <Performances user={userFromStore.userPerformances} />
+          </div>
+          <div className="score">
+            <Score user={userFromStore.userInfos.todayScore} />
+          </div>
+        </div>
+      </>) : null}
+    </>
+  );
+
+  if (status === 'loading') {
+    return (
+      <div className="dashboard">
+        <div className="loading"></div>
+        <div className="dashboard-background"></div>
+        {renderContent()}
+      </div>
+    );
+  }
+
+  if (status === 'failed') {
+    console.log('erreur');
+    return (
+      <div className="dashboard">
+        {renderContent()}
+      </div>
+    );
   }
 
   return (
     <div className="dashboard">
-        {users && userFromStore ? (
-        <>
-          <div className="dashboard__header">
-            <div className="resume">
-              <div className="infos">
-                <h1>
-                  Bonjour{" "}
-                  <span className="name">
-                    {userFromStore.userInfos.userInfos.firstName}
-                  </span>
-                </h1>
-              </div>
-              <p>Félicitations ! Vous avez explosé vos objectifs hier 👏</p>
-            </div>
-            <div className="switch">
-              <input type="checkbox" id="switch" checked={dataSource === 'api'} onChange={handleSwitch}/>
-              <label htmlFor="switch"></label>
-              Données {dataSource === 'local' ? 'locales' : 'API'}
-            </div>
-          </div>
-          <div className="ladderboard">
-            <div className="cards">
-              <Card userKeyData={userFromStore.userInfos.keyData} />
-            </div>
-            <div className="activity">
-              <ActivityGraph user={userFromStore.userActivity} />
-            </div>
-            <div className="average-sessions">
-              <AverageSessions user={userFromStore.userAverage}/>
-            </div>
-            <div className="performances">
-              <Performances user={userFromStore.userPerformances} />
-            </div>
-            <div className="score">
-              <Score user={userFromStore.userInfos.todayScore} />
-            </div>
-          </div>
-        </>
-        ): <div>Les données de l'utilisateur ne sont pas disponibles pour le moment.</div>}
+      {users && userFromStore ? renderContent() : <div>Les données de l'utilisateur ne sont pas disponibles pour le moment.</div>}
     </div>
   );
 }
