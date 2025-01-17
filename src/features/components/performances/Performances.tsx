785c794f55b0ff08e3ef9bbbc2eca8bde3b6fde7
ThomasBfrd <thomas.bfrd@gmail.com>
2025-01-17 21:44:53 +0100

    refactor: finalisation du graphique Performance

--- src/features/components/performances/Performances.tsx
+++ src/features/components/performances/Performances.tsx
@@ -8,7 +8,7 @@ import {
   UserPerformances,
 } from "../../../app/core/interfaces/user-performance";
 
-export default function Performances({user}: {user: UserPerformances}) {
+export default function Performances({ user }: { user: UserPerformances }) {
   const ref = useRef<SVGSVGElement | null>(null);
   const [selection, setSelection] = useState<null | Selection<
     SVGSVGElement | null,
@@ -16,70 +16,81 @@ export default function Performances({user}: {user: UserPerformances}) {
     null,
     undefined
   >>(null);
+  const [width, setWidth] = useState<number>(window.innerWidth);
 
+  function checkWidth(): void {
+    return window.innerWidth < 1440 ? setWidth(250) : setWidth(300);
+  }
   useEffect(() => {
-    console.log(user);
-    
-    const width = 250;
+    const handleResize = () => {
+      checkWidth();
+    };
+
+    window.addEventListener("resize", handleResize);
+    checkWidth();
+
     const height = 250;
-    const svg = d3
-      .select(ref.current)
-      .attr("width", width)
-      .attr("height", height)
-      .append("g")
-      .attr("transform", `translate(${width / 2}, ${height / 2})`); // Centre le graphique
 
     if (!selection) {
       setSelection(select(ref.current));
     } else {
-      // Ajout d'hexagones intérieurs
-      const numberOfHexagons = 5; // Nombre d'hexagones à dessiner
-      const hexagonRadius = 80; // Rayon de l'hexagone principal
-      
+      selection.selectAll("*").remove();
+      const svg = d3
+        .select(ref.current)
+        .attr("width", width)
+        .attr("height", height)
+        .append("g")
+        .attr("transform", `translate(${width / 2}, ${height / 2})`);
+
+      // Décoration polygones intérieurs
+      const numberOfHexagons = 5;
+      const hexagonRadius = 80;
+
       for (let j = 0; j < numberOfHexagons; j++) {
         const currentRadius =
-        hexagonRadius - j * (hexagonRadius / numberOfHexagons); // Rayon réduit
+          hexagonRadius - j * (hexagonRadius / numberOfHexagons);
         const hexagonPoints = user.data.map((_, i) => {
-          const angle = (i * (2 * Math.PI)) / user.data.length; // Calcul de l'angle
-          const x = currentRadius * Math.cos(angle - Math.PI / 2); // Coordonnée X
-          const y = currentRadius * Math.sin(angle - Math.PI / 2); // Coordonnée Y
+          const angle = (i * (2 * Math.PI)) / user.data.length;
+          const x = currentRadius * Math.cos(angle - Math.PI / 2);
+          const y = currentRadius * Math.sin(angle - Math.PI / 2);
           return { x, y };
         });
 
         svg
-        .append("polygon")
-        .attr("points", hexagonPoints.map((p) => `${p.x},${p.y}`).join(" "))
+          .append("polygon")
+          .attr("points", hexagonPoints.map((p) => `${p.x},${p.y}`).join(" "))
           .attr("fill", "none")
           .attr("stroke", "white")
           .attr("stroke-width", 1);
-        }
+      }
 
-              // Calculer les points pour le polygone intérieur basé sur les valeurs des métriques
+      // Polygone intérieur
       const innerPolygonPoints = user.data.map((d, i) => {
-        const angle = (i * (2 * Math.PI)) / user.data.length; // Calcul de l'angle
-        const radius = d.value / 3; // Normaliser la valeur pour le rayon
-        const x = radius * Math.cos(angle - Math.PI / 2); // Coordonnée X
-        const y = radius * Math.sin(angle - Math.PI / 2); // Coordonnée Y
+        const angle = (i * (2 * Math.PI)) / user.data.length;
+        const radius = d.value / 3;
+        const x = radius * Math.cos(angle - Math.PI / 2);
+        const y = radius * Math.sin(angle - Math.PI / 2);
         return { x, y };
       });
 
-        // Dessiner le polygone intérieur
-        svg
-          .append("polygon")
-          .attr(
-            "points",
-            innerPolygonPoints.map((p) => `${p.x},${p.y}`).join(" ")
-          )
-          .attr("fill", variables.red) // Couleur avec transparence
-          .attr("opacity", 0.8)
-          .attr("stroke", "red")
-          .attr("z-index", 15)
-          .attr("stroke-width", 2);
+      // Polygone
+      svg
+        .append("polygon")
+        .attr(
+          "points",
+          innerPolygonPoints.map((p) => `${p.x},${p.y}`).join(" ")
+        )
+        .attr("fill", variables.red)
+        .attr("opacity", 0.8)
+        .attr("stroke", "red")
+        .attr("z-index", 15)
+        .attr("stroke-width", 2);
 
-      // Ajout des labels pour chaque métrique
+      // Légendes
       user.data.forEach((d, i) => {
         const angle = (i * (2 * Math.PI)) / user.data.length;
-        const radius = 105; // Décalage pour le texte
+        // Décalage pour le texte
+        const radius = 105;
         const x = radius * Math.cos(angle - Math.PI / 2);
         const y = radius * Math.sin(angle - Math.PI / 2);
 
@@ -91,10 +102,19 @@ export default function Performances({user}: {user: UserPerformances}) {
           .attr("text-anchor", "middle")
           .text(KindTransform[d.kind])
           .style("font-size", "12px")
-          .attr("fill", "white")
+          .attr("fill", "white");
       });
     }
-  }, [selection, user]);
 
-  return <svg ref={ref} width={250} height={250} className="performances-svg"></svg>;
+    return () => {};
+  }, [selection, user, width]);
+
+  return (
+    <svg
+      ref={ref}
+      width={width}
+      height={250}
+      className="performances-svg"
+    ></svg>
+  );
 }
