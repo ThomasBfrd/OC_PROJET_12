86cfcabf8c7c2d360d5f7474829e24a4078dc41d
ThomasBfrd <thomas.bfrd@gmail.com>
2025-01-17 21:47:33 +0100

    refactor: finalisation du graphique Score

--- src/features/components/score/Score.tsx
+++ src/features/components/score/Score.tsx
@@ -1,10 +1,9 @@
 import React, { useEffect, useRef, useState } from "react";
-import { Selection } from "d3";
+import { Selection, select } from "d3";
 import * as d3 from "d3";
-import variables from "/src/assets/styles/variables.module.scss";
 import "./Score.scss";
 
-const Score = ({user}: {user: number}) => {
+const Score = ({ user }: { user: number }) => {
   const svgRef = useRef<SVGSVGElement | null>(null);
   const [selection, setSelection] = useState<null | Selection<
     SVGSVGElement | null,
@@ -12,25 +11,36 @@ const Score = ({user}: {user: number}) => {
     null,
     undefined
   >>(null);
+  const [width, setWidth] = useState<number>(window.innerWidth);
 
+  function checkWidth(): void {
+    return window.innerWidth < 1440 ? setWidth(250) : setWidth(300);
+  }
   useEffect(() => {
+    const handleResize = () => {
+      checkWidth();
+    };
+
+    window.addEventListener("resize", handleResize);
+    checkWidth();
+
     if (!selection) {
-      const svg = d3.select(svgRef.current);
-      const width = 250;
+      setSelection(select(svgRef.current));
+    } else {
+      // Suppression des enfants du SVG avant sa recréation
+      selection.selectAll("*").remove();
+
+      const svg = select(svgRef.current);
       const height = 250;
 
-      // Configuration de la zone SVG
+      // Configuration du SVG
       svg
         .attr("width", width)
         .attr("height", height)
         .append("g")
         .attr("transform", `translate(${width / 2}, ${height / 2}) rotate(90)`);
-
-      setSelection(svg);
-    } else {
       const radius = 80;
 
-      // Générateur d'arc
       const arcGenerator = d3
         .arc()
         .innerRadius(radius - 12)
@@ -38,25 +48,25 @@ const Score = ({user}: {user: number}) => {
         .cornerRadius(50)
         .startAngle(-Math.PI / 2);
 
-      // Angle de fin basé sur la valeur 64%
-      const endAngle = -Math.PI / 2 - (64 / 100) * 2 * Math.PI;
+      // Angle de fin
+      const endAngle = -Math.PI / 2 - user * 2 * Math.PI;
 
-      // Sélection du groupe (ou création si nécessaire)
       const g = selection.select("g");
 
       // Arriere Plan
       g.append("circle")
-        .attr("r", radius) // Rayon du cercle
-        .attr("fill", "white") // Couleur du fond
+        .attr("r", radius)
+        .attr("fill", "white")
         .attr("z-index", -2)
-        .attr("fill-opacity", 0.5); // Opacité pour le fond intérieur
+        .attr("fill-opacity", 0.5);
 
       const path = g.selectAll<SVGPathElement, number>("path").data([user]);
+
       path
         .enter()
         .append("path")
-        .merge(path) // Fusionne les entrées et les mises à jour
-        .attr("fill", variables.red)
+        .merge(path)
+        .attr("fill", "#E60000")
         .transition()
         .duration(1000)
         .attrTween("d", function (this: SVGPathElement) {
@@ -73,31 +83,33 @@ const Score = ({user}: {user: number}) => {
           };
         });
 
-      // Ajout du texte au centre de l'arc
+      // Ajout du texte au centre du cercle
       g.append("text")
-        .attr("x", 0) // Position X au centre
-        .attr("y", -10) // Position Y au centre
+        .attr("x", 0)
+        .attr("y", -10)
         .attr("transform", "rotate(-90)")
-        .attr("text-anchor", "middle") // Centrer le texte
-        .attr("font-size", "16px") // Taille de la police
+        .attr("text-anchor", "middle")
+        .attr("font-size", "16px")
         .attr("class", "percent")
-        .text(`${user*100}%`);
+        .text(`${user * 100}%`);
 
-      // Ajout du texte au centre de l'arc
+      // Ajout du texte au centre du cercle 2
       g.append("text")
-        .attr("y", 15) // Position Y au centre
+        .attr("y", 15)
         .attr("transform", "rotate(-90)")
         .attr("class", "details")
-        .attr("text-anchor", "middle") // Centrer le texte
-        .selectAll("tspan") // Sélectionne tous les tspans
-        .data(["de votre", "objectif"]) // Données pour les lignes
-        .enter() // Crée un tspan pour chaque ligne
-        .append("tspan") // Ajoute un tspan
-        .text((d) => d) // Définit le texte du tspan
-        .attr("x", 0) // Position X au centre
-        .attr("dy", (d, i) => (i === 0 ? 0 : 20)); // Position Y pour le retour à la ligne
+        .attr("text-anchor", "middle")
+        .selectAll("tspan")
+        .data(["de votre", "objectif"])
+        .enter()
+        .append("tspan")
+        .text((d) => d)
+        .attr("x", 0)
+        .attr("dy", (d, i) => (i === 0 ? 0 : 20));
     }
-  }, [selection]);
+
+    return () => {};
+  }, [selection, user, width]);
 
   return (
     <svg className="score-circle" ref={svgRef} width={250} height={250}></svg>
