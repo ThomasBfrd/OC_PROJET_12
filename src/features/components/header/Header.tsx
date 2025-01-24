abdd50fd2769156de4b616c9a87bd9e825fd5bcb
ThomasBfrd <thomas.bfrd@gmail.com>
2025-01-24 14:55:57 +0100

    feat: menu qui permet d'être redirigé vers la page d'accueil

--- src/features/components/header/Header.tsx
+++ src/features/components/header/Header.tsx
@@ -1,14 +1,19 @@
+import { Link } from 'react-router-dom';
 import './Header.scss';
 
 export default function Header() {
   return (
     <div className='header__menu'>
       <div className="header__menu-logo">
-        <img src="/public/logo.png" alt="logo" className='header__menu-img'/>
+        <Link to={'/home'}>
+          <img src="/public/logo.png" alt="logo" className='header__menu-img'/>
+        </Link>
       </div>
       <div className="header__menu-nav">
         <ul className="header__menu-nav-urls">
-          <li className='header__menu-nav-url'>Accueil</li>
+          <Link to={'/home'} className='link'>
+            <li className='header__menu-nav-url'>Accueil</li>
+          </Link>
           <li className='header__menu-nav-url'>Profil</li>
           <li className='header__menu-nav-url'>Réglage</li>
           <li className='header__menu-nav-url'>Communauté</li>
