43b323d9f6ee569d2252fc174cd5d39e10825956
ThomasBfrd <thomas.bfrd@gmail.com>
2025-01-24 14:55:37 +0100

    feat: création d'une page d'accueil permettant de choisir l'utilisateur

---
+++ src/features/pages/home/Home.tsx
@@ -0,0 +1,31 @@
+import React from 'react'
+import './Home.scss';
+import { useSelector } from 'react-redux';
+import { selectUsers } from '../../../app/core/selectors';
+import { UserData } from '../../../app/core/interfaces/user-infos.interface';
+import { Link } from 'react-router-dom';
+
+export default function Home() {
+
+    const users = useSelector(selectUsers)
+
+  return (
+    <div className='home'>
+        <div className="home__welcome">
+            Veuillez sélectionner un utilisateur
+        </div>
+        <div className="home__button__list">
+            {users.localAllData.data ? users.localAllData.data.map((user: UserData) => (
+                <Link to={`/dashboard/${user.id}`} key={user.id} className='link'>
+                    <div className='home__button' >
+                        <span className='home__button--text'>
+                            Profil de {user.userInfos.firstName}
+                        </span>
+                    </div>
+                </Link>
+                )) : null
+            }
+        </div>
+    </div>
+  )
+}
