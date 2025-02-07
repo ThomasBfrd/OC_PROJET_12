9d497b5e589c502b61391436917b5501b7cf8386
ThomasBfrd <thomas.bfrd@gmail.com>
2025-02-07 15:26:44 +0100

    refactor: siplification des composants avec l'utilisation du Design Pattern sur le fichier Api.tsx

--- src/app/features/pages/home/Home.tsx
+++ src/app/features/pages/home/Home.tsx
@@ -1,28 +1,27 @@
-import React from "react";
 import "./Home.scss";
-import { useSelector } from "react-redux";
-import { selectUsers } from "../../../core/selectors";
-import { UserData } from "../../../core/interfaces/user-infos.interface";
 import { Link } from "react-router-dom";
 
 export default function Home() {
-  const users = useSelector(selectUsers);
 
   return (
     <div className="home">
       <div className="home__welcome">Veuillez sélectionner un utilisateur</div>
       <div className="home__button__list">
-        {users.localAllData.data
-          ? users.localAllData.data.map((user: UserData) => (
-              <Link to={`/dashboard/${user.id}`} key={user.id} className="link">
+
+              <Link to={`/dashboard/12`}className="link">
                 <div className="home__button">
                   <span className="home__button--text">
-                    Profil de {user.userInfos.firstName}
-                  </span>
+                    Profil de Karl                  
+                    </span>
                 </div>
               </Link>
-            ))
-          : null}
+              <Link to={`/dashboard/18`}className="link">
+                <div className="home__button">
+                  <span className="home__button--text">
+                    Profil de Cécilia                  
+                    </span>
+                </div>
+              </Link>
       </div>
     </div>
   );
